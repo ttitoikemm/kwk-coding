10e0966bdd4874afe7870eaf80052724d5a795ef
ttitoikemm <maryanne270409@gmail.com>
2023-06-26 12:21:33 -0600

    Initial Commit

---
+++ kwk coding/kwk_codingApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwk_codingApp.swift
+//  kwk coding
+//
+//  Created by Otitodilichukwu Ikem on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct kwk_codingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
